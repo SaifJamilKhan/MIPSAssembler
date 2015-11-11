d5c5c1480d6045d1cf214eee1618088ae5f13e74
Saif Khan <sj5khan@uwaterloo.ca>
2015-11-11 14:17:35 -0500

    Initial Commit

---
+++ assignment3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  assignment3
+//
+//  Created by Saif Khan on 2015-11-11.
+//  Copyright © 2015 Saif Khan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
